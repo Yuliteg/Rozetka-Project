3895972c73d8a3cc81d0fe1e42b741d71f1af385
Yulia <78503488+Yuliteg@users.noreply.github.com>
2023-07-04 16:40:44 +0200

    add-host-server

--- rozetka-app/src/helpersFunction/getProd.js
+++ rozetka-app/src/helpersFunction/getProd.js
@@ -3,7 +3,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 export const fetchGoods = createAsyncThunk(
   'posts/getGoods',
   async (thunkAPI) => {
-    const result = await fetch('http://localhost:3001/products').then(
+    const result = await fetch('https://github-repo-rozetka.onrender.com/products').then(
     (data) => data.json()
   )
   return result;
